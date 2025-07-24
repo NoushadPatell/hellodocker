280dd12eca6653de064559b63d3da8e746c51d15
NoushadPatell <135932169+NoushadPatell@users.noreply.github.com>
2025-07-24 19:13:52 +0530

    Update HelloDockerController.java

--- backend/src/main/java/com/nosh/hello_world/controller/HelloDockerController.java
+++ backend/src/main/java/com/nosh/hello_world/controller/HelloDockerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://hellodocker.vercel.app/")
+@CrossOrigin(origins = "https://hellodocker.vercel.app")
 @RestController
 @RequestMapping("/api")
 public class HelloDockerController {
